1d788b3923e3e5c41e6b78832308fa0527d5289a
Nicole <nchung@live.com>
2024-02-01 14:29:40 -0800

    pin definitions

--- main/main.c
+++ main/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+// I2C (screen) — GPIO0 (scl) and GPIO1 (sda)
+// analog input (hall sensor) — GPIO4
+// digital input (button) — GPIO5
+// digital output (motor) — GPIO10
+#define DISP_SCL 0
+#define DISP_SDA 1
+#define ANALOG_IN 4
+#define DIG_IN 5
+#define DIG_OUT 10
+
 void app_main(void)
 {
 
