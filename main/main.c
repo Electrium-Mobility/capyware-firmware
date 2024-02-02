93e34fe609bb906a06471839931b769b7f644caa
Nathan <test@gmail.com>
2024-02-02 18:18:55 -0500

    Tester function for display

--- main/main.c
+++ main/main.c
@@ -7,3 +7,11 @@ void app_main(void)
         debug();
     #endif /* DEBUG == 1 */
 }
+
+void display_test(){
+    SSD1306_t dev; 
+    ssd1306_clear_screen(&dev, false); //Bool is for 'invert' parameter. No idea what that does.
+	ssd1306_contrast(&dev, 0xff); //What does this do? I don't know.
+	ssd1306_display_text_x3(&dev, 0, "Hello", 5, false); //Page number, text, text length, invert parameter
+	vTaskDelay(3000 / portTICK_PERIOD_MS); //Add a delay for some reason
+}
