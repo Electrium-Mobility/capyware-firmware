fc451bee643991442e7ce2bd98922619b4a8ef37
George Li <georgeli293@gmail.com>
2024-02-07 18:06:44 -0500

    WIP

--- main/physicalInputHandler.c
+++ main/physicalInputHandler.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-//static const char *TAG = "example";
 void setup_inputs(void)
 {
     gpio_reset_pin(BUTTON_IN);
@@ -8,7 +7,44 @@ void setup_inputs(void)
     gpio_set_direction(BUTTON_IN, GPIO_MODE_INPUT);
 }
 
-int get_digital_input(int pin_number)
+static void button_single_click_cb(void *arg,void *usr_data)
 {
-    return gpio_get_level(pin_number);
+    ESP_LOGI("Mode Changed to :", "BUTTON_SINGLE_CLICK");
 }
+
+static void button_double_click_cb(void *arg,void *usr_data)
+{
+    ESP_LOGI("Mode Changed to :", "BUTTON_DOUBLE_CLICK");
+}
+
+static void button_long_press_cb(void *arg,void *usr_data)
+{
+    ESP_LOGI("Mode Changed to :", "BUTTON_LONG_PRESS");
+}
+void button_setup(void)
+{
+    button_config_t gpio_btn_cfg = {
+        .type = BUTTON_TYPE_GPIO,
+        .long_press_ticks = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
+        .short_press_ticks = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
+        .gpio_button_config = {
+            .gpio_num = 0,
+            .active_level = 0,
+        },
+    };
+    button_handle_t gpio_btn = iot_button_create(&gpio_btn_cfg);
+    if(NULL == gpio_btn) {
+        ESP_LOGE("Button setup: ", "Button create failed");
+    }
+
+    iot_button_register_cb(gpio_btn, BUTTON_SINGLE_CLICK, button_single_click_cb,NULL);
+    iot_button_register_cb(gpio_btn, BUTTON_DOUBLE_CLICK, button_double_click_cb,NULL);
+    iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_UP, button_long_press_cb,NULL);
+}
+
+
+
+// int get_digital_input(int pin_number)
+// {
+//     return gpio_get_level(pin_number);
+// }
