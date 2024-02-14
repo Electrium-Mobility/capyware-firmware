f73c6297950a2372e7eebc6b724910110c773071
George Li <georgeli293@gmail.com>
2024-02-13 19:58:23 -0500

    fixed compile errors with physical input handler

--- main/physicalInputHandler.c
+++ main/physicalInputHandler.c
@@ -1,26 +1,20 @@
 #include "main.h"
+#include "esp_log.h"
+#include "iot_button.h"
+#include "driver/gpio.h"
 
 
 int remote_on = 1;
 int speed_level = 1;
 int forward_direction = 1;
 
-void setup_inputs(void)
-{
-    gpio_reset_pin(BUTTON_IN);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BUTTON_IN, GPIO_MODE_INPUT);
-
-    button_setup();
-}
-
-static void button_single_click_cb(void *arg,void *usr_data)
+void button_single_click_cb(void *arg, void *usr_data)
 {
     speed_level = (speed_level % 3) + 1;
     ESP_LOGI("Mode:", "Speed Level: %d", speed_level);
 }
 
-static void button_double_click_cb(void *arg,void *usr_data)
+void button_double_click_cb(void *arg,void *usr_data)
 {
     if (forward_direction){
         forward_direction = 0;
@@ -31,17 +25,22 @@ static void button_double_click_cb(void *arg,void *usr_data)
     }
 }
 
-static void button_long_press_cb(void *arg,void *usr_data)
+void button_long_press_cb(void *arg,void *usr_data)
 {
-    remote_powered_on = false;
+    remote_on = false;
     ESP_LOGI("Mode Changed to :", "BUTTON_LONG_PRESS");
 }
-void button_setup(void)
+
+void setup_inputs(void)
 {
+    gpio_reset_pin(BUTTON_IN);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BUTTON_IN, GPIO_MODE_INPUT);
+
     button_config_t gpio_btn_cfg = {
         .type = BUTTON_TYPE_GPIO,
-        .long_press_ticks = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
-        .short_press_ticks = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
+        .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
+        .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
         .gpio_button_config = {
             .gpio_num = 0,
             .active_level = 0,
@@ -57,9 +56,7 @@ void button_setup(void)
     iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_UP, button_long_press_cb,NULL);
 }
 
-
-
-// int get_digital_input(int pin_number)
-// {
-//     return gpio_get_level(pin_number);
-// }
+int get_digital_input(int pin_number)
+{
+    return gpio_get_level(pin_number);
+}
--- main/physicalInputHandler.h
+++ main/physicalInputHandler.h
@@ -1,5 +1,7 @@
-#include "driver/gpio.h"
 #include "esp_adc/adc_continuous.h"
 
 void setup_inputs(void);
 int get_digital_input(int pin_number);
+void button_single_click_cb(void *arg,void *usr_data);
+void button_double_click_cb(void *arg,void *usr_data);
+void button_long_press_cb(void *arg,void *usr_data);
